b63a708c0fc0da5e90bde8cbde8b4ca670d0bc6a
OscarSah <oscar@cybertekschool.com>
2022-06-05 18:52:52 +0300

    Review Second Hour

---
+++ liveReviewSessions/week5/DynamicSubstring.java
@@ -0,0 +1,46 @@
+package week5;
+
+public class DynamicSubstring {
+    public static void main(String[] args) {
+
+
+        String searchResult = "result count:34521";
+
+        System.out.println(searchResult.length()); // last index then 18-1= 17
+        // just taking numbers with substring
+        String numbers = searchResult.substring(13); // when you give beginning index takes the rest of the string
+        System.out.println("numbers = " + numbers);
+
+        /*
+        instead of static way: indexOf() method --> takes a string/char from you as parameter, returns the index
+         */
+
+        int columnIndex = searchResult.indexOf(':');
+        System.out.println("columnIndex = " + columnIndex);
+
+       String numbersNew = searchResult.substring(columnIndex+1);
+
+        System.out.println(numbersNew.equals(numbers));
+
+        String searchResultNew = " username:oscar  search result count: 12345 more column : more information ";
+
+    /*
+    we will use overloaded method of indexOf()
+     */
+        int firstColumnIndex = searchResultNew.indexOf(':');
+        System.out.println("firstColumnIndex = " + firstColumnIndex);
+        int secondColumnIndex = searchResultNew.indexOf(':',firstColumnIndex+1); // search starts from index 10
+        System.out.println("secondColumnIndex = " + secondColumnIndex);
+
+        // Where the number starts = secondColumnIndex  + 2
+        // where the number finishes = secondColumnIndex  + 7
+
+        String numbers3 = searchResultNew.substring(secondColumnIndex+2,secondColumnIndex+7);
+        System.out.println("numbers3 = " + numbers3);
+
+
+
+
+
+    }
+}
--- liveReviewSessions/week5/Palindrome.java
+++ liveReviewSessions/week5/Palindrome.java
@@ -17,6 +17,9 @@ public class Palindrome {
          */
 
         // create an empty container for my result
+
+        word = word.toLowerCase();  // to make our result of the task case insensitive
+
         String result = "";
         result += word.charAt(4);  // instead of writing 4: length()-1
         result += word.charAt(3);
@@ -31,7 +34,9 @@ public class Palindrome {
             System.out.println("They are Palindrome");
         }
 
-
+// Kadir's Solution:
+        String str = "kazak"; // str.charAt(0): k  = str.charAt(4) : k
+        if (str.charAt(0)==str.charAt(4)&&str.charAt(1)==str.charAt(3)) System.out.println("Palindrome");
 
     }
 }
