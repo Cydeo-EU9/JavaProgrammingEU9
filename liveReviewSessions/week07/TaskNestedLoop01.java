089fa20ead026ba83e80f49c521b1e3ef164f728
OscarSah <oscar@cybertekschool.com>
2022-06-26 18:21:26 +0300

    Nested Loop Task

---
+++ liveReviewSessions/week07/TaskNestedLoop01.java
@@ -0,0 +1,33 @@
+package week07;
+
+public class TaskNestedLoop01 {
+    /*
+    rowCount:5
+    columnCount:5
+    1                               : rowCount-1
+    2 "4" 6                         : rowCount-1, rowCount-2
+    3 "4" 7 "3" 10                  : rowCount-1, rowCount-2, rowCount-3
+    4 "4" 8 "3" 11 "2" 13
+    5 "4" 9 "3" 12 "2" 14 "1" 15
+
+    What is repeating?
+    How is it repeating?
+    Algorithm:
+    Nested Loop
+    Outer loop should handle rows
+    inner loop should handle columns
+
+   */
+
+    public static void main(String[] args) {
+        int rowCount = 10;
+        for(int row=1; row<=rowCount;row++){
+            int value = row;
+            for(int col = 1; col<=row ; col++){
+                System.out.print(value+" ");
+                value += rowCount - col;
+            }
+            System.out.println();
+        }
+    }
+}
