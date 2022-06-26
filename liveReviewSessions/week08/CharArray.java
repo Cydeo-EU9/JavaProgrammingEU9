88d5a1ebbd6b21e5c2edebba397a2946a7a5f9a6
OscarSah <oscar@cybertekschool.com>
2022-06-26 22:47:57 +0300

    Char Array

--- liveReviewSessions/week08/CharArray.java
+++ liveReviewSessions/week08/CharArray.java
@@ -18,5 +18,37 @@ public class CharArray
         for (char newLetter : newLetters) {
             System.out.print(newLetter+" ");
         }
+
+        /*
+        find the frequency of chars in a string
+         */
+
+        System.out.println();
+        String str = "aaabbcdddeffff";
+        char[] arr = str.toCharArray();
+
+        String result = "";
+
+        for (int i = 0; i < arr.length; i++) {
+            char c = arr[i];
+
+            int count = 0;
+
+            for (int j = 0; j < arr.length; j++) {
+                if(c == arr[j]){
+                    count++;
+                }
+             }
+            if(result.contains(""+c)){
+                continue;  // if my result string has this char, go to beginning of loop
+            }
+
+            result+=c;
+            result+=count;
+
+        }
+        System.out.println("result = " + result);
+
+
     }
 }
