3455b4df276af61a10c372ba5ae6d60fa972070f
OscarSah <oscar@cybertekschool.com>
2022-07-17 21:21:48 +0300

    Review Week 10 Method Overloading

---
+++ liveReviewSessions/week10/LoginTest.java
@@ -0,0 +1,32 @@
+package week10;
+
+public class LoginTest {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean login(String username, String password){
+
+        if(username.equalsIgnoreCase("cydeoStudent") && password.equalsIgnoreCase("cydeoPassword")){
+            return true;
+        }
+        return false;
+    }
+
+ //   public static String login(String username, String password){ } NOT METHOD OVERLOADING
+
+    public static boolean login(){
+        if(getUsername().equalsIgnoreCase("cydeoStudent") && getPassword().equalsIgnoreCase("cydeoPassword")){
+            return true;
+        }
+        return false;
+    }
+
+    public static String getPassword(){
+        // assume that we are getting this piece of info from an external file
+        return "cydeoPassword";
+    }
+    public static String getUsername(){
+        return "cydeoStudent";
+    }
+}
