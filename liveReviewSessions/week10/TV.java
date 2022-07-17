836f5865670a49ff0aaa3f869342b81301ad67f1
OscarSah <oscar@cybertekschool.com>
2022-07-17 22:54:01 +0300

    Review Week 10 TV class created

---
+++ liveReviewSessions/week10/TV.java
@@ -0,0 +1,15 @@
+package week10;
+
+public class TV {
+    public int channel;   // default value to 1   --- > Instance variables belong to OBJECTS
+    public int volumeLevel; // default value to 1
+    public boolean on; // default value is OFF
+
+   public TV(){
+       channel = 1;
+       volumeLevel = 1;
+       on= false;
+   }
+
+
+}
