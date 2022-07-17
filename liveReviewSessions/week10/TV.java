a6c41b543bb03e85a2fd69bfe3062ab986a7237b
OscarSah <oscar@cybertekschool.com>
2022-07-17 23:04:36 +0300

    Review Week 10 TV class created

--- liveReviewSessions/week10/TV.java
+++ liveReviewSessions/week10/TV.java
@@ -1,6 +1,7 @@
 package week10;
 
 public class TV {
+    // attributes
     public int channel;   // default value to 1   --- > Instance variables belong to OBJECTS
     public int volumeLevel; // default value to 1
     public boolean on; // default value is OFF
@@ -10,6 +11,9 @@ public class TV {
        volumeLevel = 1;
        on= false;
    }
-
+// action
+   public void turnOn(){
+       on = true;
+   }
 
 }
---
+++ liveReviewSessions/week10/TVtest.java
@@ -0,0 +1,29 @@
+package week10;
+
+public class TVtest {
+    public static void main(String[] args) {
+        
+        // create object or instance of TV class
+        
+        TV samsung = new TV();
+
+        System.out.println("samsung.volumeLevel = " + samsung.volumeLevel);
+
+        samsung.volumeLevel = 6;
+        System.out.println("samsung.volumeLevel = " + samsung.volumeLevel);
+
+        
+        TV sony = new TV(); // creating a new object from TV class
+
+        System.out.println("sony.volumeLevel = " + sony.volumeLevel); // 1
+
+        System.out.println("sony.on = " + sony.on);
+        sony.turnOn();
+        System.out.println("sony.on = " + sony.on);
+        
+        
+        
+        
+        
+    }
+}
