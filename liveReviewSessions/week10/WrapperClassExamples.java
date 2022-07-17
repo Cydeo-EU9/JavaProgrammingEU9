b10b3bae0ac6e766174fdd5959738e2c268ea5a1
OscarSah <oscar@cybertekschool.com>
2022-07-17 21:53:19 +0300

    Review Week 10 Wrapper

---
+++ liveReviewSessions/week10/WrapperClassExamples.java
@@ -0,0 +1,34 @@
+package week10;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class WrapperClassExamples {
+    public static void main(String[] args) {
+
+        int i1 = 100; // primitive
+
+        Integer i2; // object   Scanner scan = new Scanner...
+
+        // auto-boxing Examples
+        i2= i1; // automatically the data enlarge itself from primitive to Object
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(i1);// automatically the data enlarge itself from primitive to Object
+        arrayList.add(i2);
+        System.out.println("---Arraylist---");
+        System.out.println("arrayList = " + arrayList);
+
+        Integer n1 = 300;
+        int n2 = n1; // un-boxing to primitive
+        n2 = arrayList.get(1); // getting an Object from arrayList and assigning to primitive : un-boxing
+
+        String searchResult = "1-48 of over 6000 results for java";
+        String[] arr = searchResult.split(" ");
+        System.out.println(Arrays.toString(arr));
+
+        int resultAsPrimitive = Integer.parseInt(arr[3]);
+        System.out.println(resultAsPrimitive+1);
+
+    }
+}
