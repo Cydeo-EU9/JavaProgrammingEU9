da0b53cfc24fa93989afae10f2dc9577e87e8447
OscarSah <oscar@cybertekschool.com>
2022-07-31 19:30:43 +0300

    Book

---
+++ liveReviewSessions/week13/inheritance/Book.java
@@ -0,0 +1,29 @@
+package week13.inheritance;
+
+public class Book {
+    String title,type,author;
+    double price;
+
+    public Book(String title, String type, String author, double price) {
+        this.title = title;
+        this.type = type;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book() {
+    }
+
+    public void buy(String store){
+        System.out.println("Buying it from :"+store);
+    }
+    public void borrow(String source){
+        System.out.println("Borrowing it from :"+source);
+    }
+
+    public void lend(String user){
+        System.out.println("Borrowing it from :"+user);
+    }
+
+
+}
