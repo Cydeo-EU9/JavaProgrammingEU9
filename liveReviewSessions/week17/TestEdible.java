402abb58d96d1d680c44036e1ad5d5e1e1e32e24
OscarSah <oscar@cybertekschool.com>
2022-08-28 22:25:28 +0300

    Week 17 Review

--- liveReviewSessions/week17/TestEdible.java
+++ liveReviewSessions/week17/TestEdible.java
@@ -3,6 +3,26 @@ package week17;
 public class TestEdible {
     public static void main(String[] args) {
 
+
+          //  Object[] objects = {new Tiger(), new Chicken(), new Apple()};
+          // ((Tiger)objects[0])
+
+           // instanceof : this checks if an object belongs to a certain class--> Returns boolean
+Tiger tiger = new Tiger();
+Chicken chicken = new Chicken();
+Apple apple = new Apple();
+
+            // store objects in a data structure: Array
+        Object[] objects = {tiger,chicken,apple}; // same with {new Tiger(), new Chicken(), new Apple()}
+
+        tiger.sound();
+        chicken.sound();
+        chicken.howToEat();  // coming from interface
+        apple.howToEat(); // interface
+
+        Edible.staticMethod();  // how we call a static method, with Interface name
+       // Chicken.staticMethod();  static classes belongs to the classes or interfaces
+        chicken.someMethod(); // I am calling default method with object name
     }
 }
 
@@ -41,12 +61,17 @@ class Tiger extends Animal{
     }
 }
 
-abstract class Fruit implements Edible{ // implementing abstract methods in interface is optional now
+abstract class Fruit implements Edible,moreInterface{ // implementing abstract methods in interface is optional now
 }
 class Apple extends Fruit{
  @Override
     public String howToEat() {
         return "Make apple cider and drink";
+    }
+
+    @Override
+    public void someMethod2() {
+
     }
 }
 class Orange extends Fruit{
@@ -55,10 +80,20 @@ class Orange extends Fruit{
         return "Orange: make orange juice";
     }
 
+    @Override
+    public void someMethod2() {
+
+    }
+
 //    @Override
 //    public void someMethod(){
 //
 //    }
+
+
+}
+interface moreInterface{
+    void someMethod2();
 }
 
 
